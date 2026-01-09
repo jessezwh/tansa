901791708b051cb7001d442ea866f98c52e72f08
Jesse Wanghan <jesse.wanghan@serato.com>
2026-01-09 13:23:20 +1300

    setup and config changes for new sever

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250401_125407_Events from './20250401_125407_Events';
 import * as migration_20250401_125547_events from './20250401_125547_events';
+import * as migration_20260105_023042 from './20260105_023042';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250401_125547_events.up,
     down: migration_20250401_125547_events.down,
-    name: '20250401_125547_events'
+    name: '20250401_125547_events',
+  },
+  {
+    up: migration_20260105_023042.up,
+    down: migration_20260105_023042.down,
+    name: '20260105_023042'
   },
 ];
